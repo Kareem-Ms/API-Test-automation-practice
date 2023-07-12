d74ecd001a2d4c21d8d9928b503d96e1ba7eae30
Kareeme <Usama@ENCAP.LOCAL>
2023-07-12 09:12:55 +0300

    Add delete test case

--- src/main/java/ContactList/apis/DeleteUserApi.java
+++ src/main/java/ContactList/apis/DeleteUserApi.java
@@ -1,4 +1,23 @@
 package ContactList.apis;
 
+import Utiles.ApiActions;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import java.util.HashMap;
+
 public class DeleteUserApi {
+    ApiActions actions;
+    String url = "https://thinking-tester-contact-list.herokuapp.com/users/me";
+
+    public DeleteUserApi() {
+        actions = new ApiActions(url);
+    }
+
+    public Response deleteUser(String token , int expectedStatusCode){
+        HashMap<String,String> header = new HashMap<>();
+        header.put("Authorization","Bearer "+token);
+
+       return actions.performRequest("DELETE", expectedStatusCode, ContentType.JSON,header,null,null,null);
+    }
 }
--- src/test/java/ContactList/tests/DeleteUserTest.java
+++ src/test/java/ContactList/tests/DeleteUserTest.java
@@ -1,4 +1,56 @@
 package ContactList.tests;
 
+import ContactList.PojoClasses.User;
+import ContactList.apis.AddUserApi;
+import ContactList.apis.DeleteUserApi;
+import ContactList.apis.LoginApii;
+import Utiles.JsonFileManager;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DeleteUserTest {
+
+    ///////////////Variables\\\\\\\\\\\\\\\\\
+    AddUserApi addUserApi;
+    DeleteUserApi deleteUserApi;
+    LoginApii loginApi;
+    JsonFileManager testData;
+    String email;
+    String password;
+    User user;
+    String currentTime = new SimpleDateFormat("ddMMyyyyHHmmssSSS").format(new Date());
+
+    ///////////////Tests\\\\\\\\\\\\\\\\\\\\\\
+    @Test
+    public void VerifyAddingUserWithUnregisteredEmail(){
+        email = testData.getTestData("UserInfo.email")+"_"+currentTime+testData.getTestData("UserInfo.domain");
+        password = testData.getTestData("UserInfo.password");
+
+        user = addUserApi.AddUser(email,testData.getTestData("UserInfo.firstName")
+                ,testData.getTestData("UserInfo.lastName")
+                ,password,201).as(User.class);
+
+        Assert.assertEquals(user.getUser().getEmail(),email);
+        Assert.assertNotNull(user.getToken());
+    }
+
+    @Test(dependsOnMethods = "VerifyAddingUserWithUnregisteredEmail")
+    public void VerifyDeletingUserSuccessfully(){
+        deleteUserApi.deleteUser(user.getToken(),200);
+
+        loginApi.Login(user.getUser().getEmail(), password, 401);
+    }
+
+    ///////////////Configuration\\\\\\\\\\\\\\\
+    @BeforeClass
+    public void setUp(){
+        testData = new JsonFileManager("src/test/resources/TestData/ContactListTestData/AddUserTestData.json");
+        addUserApi = new AddUserApi();
+        deleteUserApi = new DeleteUserApi();
+        loginApi = new LoginApii();
+    }
 }
